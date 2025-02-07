c531dff8f106a42dbd4efbb61cade373ea46fe37
BONGO-BYTES <jamesfmajura@gmail.com>
2025-02-07 18:48:07 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,1 +1,17 @@
-print('Hello there this is the updated python file')
+import requests
+version = 0
+
+txturl = 'https://raw.githubusercontent.com/BONGO-BYTES/update/refs/heads/main/update.txt'
+pyurl = 'https://raw.githubusercontent.com/BONGO-BYTES/update/refs/heads/main/update.py'
+
+py = requests.get(py)
+txt = requests.get(txturl)
+
+
+updateFile = link.text
+if int(updateFile) > version:
+    with open('update.py', 'w') as file:
+      file.write(py)
+      
+else:
+    print('No update found')
